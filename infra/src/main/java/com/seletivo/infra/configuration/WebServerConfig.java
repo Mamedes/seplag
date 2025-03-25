db4602004712aaa82f7783959b86577d6a696f71
mamedes <mamedeser@gmail.com>
2025-03-24 21:10:59 -0300

    swagger

--- infra/src/main/java/com/seletivo/infra/configuration/WebServerConfig.java
+++ infra/src/main/java/com/seletivo/infra/configuration/WebServerConfig.java
@@ -2,8 +2,17 @@ package com.seletivo.infra.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@ComponentScan("com.seplag.seletivo")
-public class WebServerConfig {
+@ComponentScan("com.seplag.seletivo.infra")
+public class WebServerConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/swagger-ui.html**")
+                .addResourceLocations("classpath:/META-INF/resources/swagger-ui.html");
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
 }
