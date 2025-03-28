7a7711d69854c3ce73548e813767d882f24d984f
Mamedes Eron <mamedeser@gmail.com>
2025-03-28 20:52:01 -0300

    Update minio-init.sh

--- minio/minio-init.sh
+++ minio/minio-init.sh
@@ -1,13 +1,32 @@
 #!/bin/sh
 
-echo "Setting permissions for MinIO initialization script..."
-chmod +x /minio/minio-init.sh
+echo "Aguardando MinIO iniciar..."
+sleep 10
+COUNTER=0
+MAX_RETRIES=20
 
-echo "Waiting for MinIO server to start..."
-sleep 5
+while ! mc alias set myminio http://minio:9000 admin sF022A-?=sZT; do
+  echo "MinIO ainda não está pronto... aguardando..."
+  sleep 2
+  COUNTER=$((COUNTER + 1))
+  if [ "$COUNTER" -ge "$MAX_RETRIES" ]; then
+    echo "Erro: MinIO não iniciou após várias tentativas."
+    exit 1
+  fi
+done
 
-echo "Executing MinIO initialization..."
-/minio/minio-init.sh &
+echo "MinIO iniciado! Configurando MinIO Client..."
 
-echo "Starting MinIO server..."
-exec minio server --console-address :9001 /data
+# Criar bucket se não existir
+if mc ls myminio/seletivo >/dev/null 2>&1; then
+  echo "Bucket 'seletivo' já existe."
+else
+  echo "Criando bucket 'seletivo'..."
+  mc mb myminio/seletivo
+  if [ $? -eq 0 ]; then
+    echo "Bucket 'seletivo' criado com sucesso!"
+  else
+    echo "Erro ao criar bucket!"
+    exit 1
+  fi
+fi
