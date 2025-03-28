30f8e1de7bb42717f694c5a78dd5e394eba077f0
Mamedes Eron <mamedeser@gmail.com>
2025-03-28 16:55:41 -0400

    add target

---
+++ infra/target/classes/db/migration/V1__create_pessoa_table.sql
@@ -0,0 +1,10 @@
+CREATE SEQUENCE IF NOT EXISTS pessoa_id_seq START WITH 1000 INCREMENT BY 1;
+
+CREATE TABLE pessoa (
+    pes_id INT PRIMARY KEY DEFAULT nextval('pessoa_id_seq'),
+    pes_nome VARCHAR(200) NOT NULL,
+    pes_data_nascimento DATE,
+    pes_sexo VARCHAR(9),
+    pes_mae VARCHAR(200),
+    pes_pai VARCHAR(200)
+);
