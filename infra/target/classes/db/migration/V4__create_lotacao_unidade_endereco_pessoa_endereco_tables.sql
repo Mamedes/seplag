30bb565cebec205107b1c7860e816a27cdf195e2
Mamedes Eron <mamedeser@gmail.com>
2025-03-31 15:30:45 -0400

    target

---
+++ infra/target/classes/db/migration/V4__create_lotacao_unidade_endereco_pessoa_endereco_tables.sql
@@ -0,0 +1,29 @@
+CREATE SEQUENCE IF NOT EXISTS lotacao_id_seq START WITH 6000 INCREMENT BY 1;
+
+CREATE TABLE lotacao (
+    lot_id INT PRIMARY KEY DEFAULT nextval('lotacao_id_seq'),
+    pes_id INT NOT NULL,
+    unid_id INT NOT NULL,
+    lot_data_lotacao DATE NOT NULL,
+    lot_data_remocao DATE,
+    lot_portaria VARCHAR(100),
+    CONSTRAINT fk_lotacao_pessoa FOREIGN KEY (pes_id) REFERENCES pessoa (pes_id) ON DELETE CASCADE,
+    CONSTRAINT fk_lotacao_unidade FOREIGN KEY (unid_id) REFERENCES unidade (unid_id) ON DELETE CASCADE
+);
+
+CREATE TABLE unidade_endereco (
+    unid_id INT NOT NULL,
+    end_id INT NOT NULL,
+    PRIMARY KEY (unid_id, end_id),
+    CONSTRAINT fk_unidade_endereco_unidade FOREIGN KEY (unid_id) REFERENCES unidade (unid_id) ON DELETE CASCADE,
+    CONSTRAINT fk_unidade_endereco_endereco FOREIGN KEY (end_id) REFERENCES endereco (end_id) ON DELETE CASCADE
+);
+
+CREATE TABLE pessoa_endereco (
+    pes_id INT NOT NULL,
+    end_id INT NOT NULL,
+    PRIMARY KEY (pes_id, end_id),
+    CONSTRAINT fk_pessoa_endereco_pessoa FOREIGN KEY (pes_id) REFERENCES pessoa (pes_id) ON DELETE CASCADE,
+    CONSTRAINT fk_pessoa_endereco_endereco FOREIGN KEY (end_id) REFERENCES endereco (end_id) ON DELETE CASCADE
+);
+
